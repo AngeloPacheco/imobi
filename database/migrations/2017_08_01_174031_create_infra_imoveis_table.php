afde9fbf49b1732ead3018910e43969970f0e09f
Angelo <angelo@angelopacheco.com.br>
2017-08-03 09:19:28 -0300

    novas

--- database/migrations/2017_08_01_174031_create_infra_imoveis_table.php
+++ database/migrations/2017_08_01_174031_create_infra_imoveis_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateInfrasTable extends Migration
+class CreateInfraImoveisTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,9 +13,8 @@ class CreateInfrasTable extends Migration
      */
     public function up()
     {
-        Schema::create('infras', function (Blueprint $table) {
+        Schema::create('infra_imoveis', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('descricao',45);
             $table->timestamps();
         });
     }
@@ -27,6 +26,6 @@ class CreateInfrasTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('infras');
+        Schema::dropIfExists('infra_imoveis');
     }
 }
